6efdf6cec0b49a20ecd670201ead686690dcb904
dejie.huang <dejie.huang@zhiren360.com>
2018-08-09 16:40:14 +0800

    新增

--- fanstasy-web/src/main/java/com/fanstasy/FanstasyApplication.java
+++ fanstasy-web/src/main/java/com/fanstasy/FanstasyApplication.java
@@ -2,11 +2,13 @@ package com.fanstasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by huangdejie on 2018/8/9 0009.
  */
 @SpringBootApplication
+@EnableConfigServer
 public class FanstasyApplication {
 
     public static void main(String[] args) {
