fedf9e8435bce7f5b8ecbbf4b498bad2ae897ad2
dejie.huang <dejie.huang@zhiren360.com>
2018-08-09 10:40:13 +0800

    init

---
+++ fanstasy-web/src/main/java/com/fanstasy/FanstasyApplication.java
@@ -0,0 +1,16 @@
+package com.fanstasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by huangdejie on 2018/8/9 0009.
+ */
+@SpringBootApplication
+public class FanstasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanstasyApplication.class,args);
+    }
+
+}
